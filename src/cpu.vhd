d6fc12330424109b4623d54125de5db447510ead
NickSettler <moiseevnikita14@gmail.com>
2022-10-12 19:40:11 +0200

    (feat) adds PC process

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -56,5 +56,20 @@ architecture behavioral of cpu is
   signal current_state : fsm_state;               -- current FSM state
   signal next_state : fsm_state;                  -- next FSM state
 begin
+  -- Program counter process
+  pc_counter: process(CLK, RESET, pc_ld, pc_inc)
+  begin
+    if RESET = '1' then
+      pc_reg <= (others => '0');
+    elsif rising_edge(CLK) then
+      if pc_inc = '1' then
+        pc_reg <= pc_reg + 1;
+      else
+        pc_reg <= (others => '0');
+      end if;
+    end if;
+  end process pc_counter;
+
+  -- DATA_ADDR <= pc_reg when pc_abus = '1' else (others => 'Z');
 
 end behavioral;
