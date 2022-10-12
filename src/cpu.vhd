3f0d035d42065a0343c190b00b672d7867fa11c1
NickSettler <moiseevnikita14@gmail.com>
2022-10-12 19:39:37 +0200

    (refactor) changes formatting

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -12,30 +12,29 @@ use ieee.std_logic_unsigned.all;
 --                        Entity declaration
 -- ----------------------------------------------------------------------------
 entity cpu is
- port (
-   CLK   : in std_logic;  -- hodinovy signal
-   RESET : in std_logic;  -- asynchronni reset procesoru
-   EN    : in std_logic;  -- povoleni cinnosti procesoru
+  port (
+    CLK   : in std_logic;  -- hodinovy signal
+    RESET : in std_logic;  -- asynchronni reset procesoru
+    EN    : in std_logic;  -- povoleni cinnosti procesoru
  
-   -- synchronni pamet RAM
-   DATA_ADDR  : out std_logic_vector(12 downto 0); -- adresa do pameti
-   DATA_WDATA : out std_logic_vector(7 downto 0); -- mem[DATA_ADDR] <- DATA_WDATA pokud DATA_EN='1'
-   DATA_RDATA : in std_logic_vector(7 downto 0);  -- DATA_RDATA <- ram[DATA_ADDR] pokud DATA_EN='1'
-   DATA_RDWR  : out std_logic;                    -- cteni (0) / zapis (1)
-   DATA_EN    : out std_logic;                    -- povoleni cinnosti
+    -- synchronni pamet RAM
+    DATA_ADDR  : out std_logic_vector(12 downto 0); -- adresa do pameti
+    DATA_WDATA : out std_logic_vector(7 downto 0); -- mem[DATA_ADDR] <- DATA_WDATA pokud DATA_EN='1'
+    DATA_RDATA : in std_logic_vector(7 downto 0);  -- DATA_RDATA <- ram[DATA_ADDR] pokud DATA_EN='1'
+    DATA_RDWR  : out std_logic;                    -- cteni (0) / zapis (1)
+    DATA_EN    : out std_logic;                    -- povoleni cinnosti
    
-   -- vstupni port
-   IN_DATA   : in std_logic_vector(7 downto 0);   -- IN_DATA <- stav klavesnice pokud IN_VLD='1' a IN_REQ='1'
-   IN_VLD    : in std_logic;                      -- data platna
-   IN_REQ    : out std_logic;                     -- pozadavek na vstup data
+    -- vstupni port
+    IN_DATA   : in std_logic_vector(7 downto 0);   -- IN_DATA <- stav klavesnice pokud IN_VLD='1' a IN_REQ='1'
+    IN_VLD    : in std_logic;                      -- data platna
+    IN_REQ    : out std_logic;                     -- pozadavek na vstup data
    
-   -- vystupni port
-   OUT_DATA : out  std_logic_vector(7 downto 0);  -- zapisovana data
-   OUT_BUSY : in std_logic;                       -- LCD je zaneprazdnen (1), nelze zapisovat
-   OUT_WE   : out std_logic                       -- LCD <- OUT_DATA pokud OUT_WE='1' a OUT_BUSY='0'
- );
+    -- vystupni port
+    OUT_DATA : out  std_logic_vector(7 downto 0);  -- zapisovana data
+    OUT_BUSY : in std_logic;                       -- LCD je zaneprazdnen (1), nelze zapisovat
+    OUT_WE   : out std_logic                       -- LCD <- OUT_DATA pokud OUT_WE='1' a OUT_BUSY='0'
+  );
 end cpu;
-
 
 -- ----------------------------------------------------------------------------
 --                      Architecture declaration
@@ -44,4 +43,3 @@ architecture behavioral of cpu is
 begin
 
 end behavioral;
-
